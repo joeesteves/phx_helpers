9d0d4db584b7b6629c7424551454a3f6249bfd20
Joe Esteves <joe@caravana.dev>
2022-03-23 01:04:51 -0300

    better b5 implementations. works with liveview

--- lib/bootstrap_v5.ex
+++ lib/bootstrap_v5.ex
@@ -3,6 +3,7 @@ defmodule PhxHelpers.BootstrapV5 do
   import Phoenix.HTML.Link
   import Phoenix.HTML.Tag
   import Phoenix.LiveView.Helpers
+  use Phoenix.Component
 
   @doc """
     maps every field_input from HTML.Form
@@ -15,65 +16,95 @@ defmodule PhxHelpers.BootstrapV5 do
     text_input
     )a
 
+  @wrapper_base_class "mb-3"
+  @label_base_class "form-label"
+  @input_base_class "form-control"
+
   for src_name <- @fields do
     dest_name = "b5_#{src_name}" |> String.to_atom()
 
-    def unquote(dest_name)(%{form: f, label: label_name, field: field} = assigns) do
+    def unquote(dest_name)(f, field, opts \\ []) do
       original_fx = unquote(src_name)
+      input_base_class = unquote(@input_base_class)
+      label_base_class = unquote(@label_base_class)
 
-      ~H"""
-      <div class={["mb-3", assigns[:div_class]]}>
-        <%= label f, label_name, class: "form-label" %>
-        <%= apply(Phoenix.HTML.Form, original_fx, [f, field, build_opts(f, field, assigns, "form-control")]) %>
-        <%= b5_error_tag f, field %>
-      </div>
-      """
+      content = [
+        label(f, opts[:label], class: label_base_class),
+        apply(Phoenix.HTML.Form, original_fx, [
+          f,
+          field,
+          build_opts(f, field, opts, input_base_class)
+        ]),
+        b5_error_tag(f, field)
+      ]
+
+      content_tag(:div, content, class: compact([@wrapper_base_class, opts[:wrapper_class]]))
     end
   end
 
-  def b5_checkbox(%{form: f, label: label_name, field: field} = assigns) do
-    ~H"""
-    <div class="mb-3 form-check">
-      <%= checkbox f, field, class: "form-check-input" %>
-      <%= label f, field, label_name, class: "form-check-label" %>
-    </div>
-    """
+  defp compact(list) do
+    Enum.reject(list, &is_nil(&1))
+    |> Enum.join(" ")
   end
 
-  def b5_select(%{form: f, label: label_name, field: field, options: options} = assigns) do
-    ~H"""
-    <div class="mb-3 ">
-      <%= label f, label_name, class: "form-label" %>
-      <%= select f, field, options, class: form_control_class("form-select", f, field, assigns)  %>
-      <%= b5_error_tag f, field %>
-    </div>
-    """
+  def b5_checkbox(f, field, opts \\ []) do
+    content = [
+      label(f, opts[:label], class: "form-check-label"),
+      checkbox(f, field, build_opts(f, field, opts, "form-check-input")),
+      b5_error_tag(f, field)
+    ]
+
+    content_tag(:div, content, class: compact([@wrapper_base_class, opts[:wrapper_class]]))
   end
 
-  def b5_submit(%{label: label_name} = assigns) do
-    ~H"""
-    <div class="mt-3">
-      <%= submit label_name, class: "btn btn-primary me-3 #{assigns[:class]}" %>
-    </div>
-    """
+  def b5_select(f, field, options, opts \\ []) do
+    content = [
+      label(f, opts[:label], class: @label_base_class),
+      select(f, field, options, build_opts(f, field, opts, "form-select")),
+      b5_error_tag(f, field)
+    ]
+
+    content_tag(:div, content, class: compact([@wrapper_base_class, opts[:wrapper_class]]))
   end
 
-  def b5_submit_cancel(
-        %{label: label_name, cancel_label: cancel_label, cancel_path: cancel_path} = assigns
-      ) do
-    ~H"""
-    <div class="mt-3">
-      <%= submit label_name, class: "btn btn-primary me-3 #{assigns[:class]}" %>
-      <%= link cancel_label, to: cancel_path %>
-    </div>
-    """
+  def b5_submit(label, opts \\ []) do
+    content = submit(label, class: compact(["btn btn-primary me-3", opts[:class]]))
+
+    content_tag(:div, content, class: compact([@wrapper_base_class, opts[:wrapper_class]]))
   end
 
-  defp build_opts(form, field, assigns, default_field_class) do
-    extra_opts = Map.drop(assigns, [:form, :field, :class, :required]) |> Enum.into([])
+  def b5_submit_and_back_to(label, opts \\ []) do
+    {back_to, opts} =
+      pop_required_option!(opts, :back_to, "expected non-nil value for :back_to in link/2")
 
-    [required: required(assigns), class: form_control_class(default_field_class, form, field, assigns)]
-    |> Keyword.merge(extra_opts)
+    {back_to_label, opts} =
+      pop_required_option!(
+        opts,
+        :back_to_label,
+        "expected non-nil value for :back_to_label in link/2"
+      )
+
+    content = [
+      submit(label, class: compact(["btn btn-primary me-3", opts[:class]])),
+      link(back_to_label, to: back_to)
+    ]
+
+    content_tag(:div, content, class: compact([@wrapper_base_class, opts[:wrapper_class]]))
+  end
+
+  defp pop_required_option!(opts, key, error_message) do
+    {value, opts} = Keyword.pop(opts, key)
+
+    unless value do
+      raise ArgumentError, error_message
+    end
+
+    {value, opts}
+  end
+
+  defp build_opts(form, field, opts, default_field_class) do
+    [class: form_control_class(default_field_class, form, field, opts)]
+    |> Keyword.merge(opts)
   end
 
   defp form_control_class(default_class, form, field, assigns) do
@@ -81,17 +112,15 @@ defmodule PhxHelpers.BootstrapV5 do
       case form do
         %{source: %Ecto.Changeset{action: action}} when action in ~w(insert update)a ->
           (Keyword.get_values(form.errors, field) |> Enum.any?() && "is-invalid") || "is-valid"
-        _ -> ""
+
+        _ ->
+          nil
       end
 
-    ~s"#{default_class} #{valid_class} #{assigns[:class]}"
+    [default_class, valid_class, assigns[:class]] |> Enum.reject(&is_nil(&1)) |> Enum.join(" ")
   end
 
-  defp required(assigns) do
-    assigns[:required] || false
-  end
-
-  def b5_error_tag(form, field) do
+  defp b5_error_tag(form, field) do
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
       content_tag(:div, b5_translate_error(error),
         class: "invalid-feedback d-block",
@@ -100,7 +129,7 @@ defmodule PhxHelpers.BootstrapV5 do
     end)
   end
 
-  def b5_translate_error({msg, opts}) do
+  defp b5_translate_error({msg, opts}) do
     # When using gettext, we typically pass the strings we want
     # to translate as a static argument:
     #
@@ -124,4 +153,8 @@ defmodule PhxHelpers.BootstrapV5 do
       Gettext.dgettext(PhxHelpers.get_config(:gettext_module), "errors", msg, opts)
     end
   end
+end
+
+defimpl Phoenix.HTML.Safe, for: Map do
+  def to_iodata(data), do: data |> Jason.encode!() |> Plug.HTML.html_escape()
 end
